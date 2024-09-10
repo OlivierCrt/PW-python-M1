dde00776e00d0d1fe762244b115f228baa799ceb
olivier <ocrampette@gmail.com>
2024-09-10 15:30:02 +0200

    end 10/09

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,14 @@
-print
+def maxValueInDict(dict_p):
+    if not dict_p : 
+        return None,None
+    res= 0
+    for i in dict_p.values() :
+        if i > res :
+            res = i
+
+    for j in dict_p.keys() :
+        if dict_p[j] == res :
+            res_j = j
+            break
+
+    return j,res
--- tp1_Crampette_Olivier.py
+++ tp1_Crampette_Olivier.py
@@ -7,22 +7,61 @@ text = t.read().decode('utf-8')
 
 
 
-def wordCount(text_p,word) :
+def wordCount(text_p) :
+    res=0
+    resdict={}
     text_temp = text_p
-
-    for ch in [',','.',';','!','?',':',"'"] :
+    for ch in [',','.',';','!','?',':',"'",'-','(',')','/','*'] :
         if ch in text_temp :
             text_temp =text_temp.replace(ch," ")
 
-    print(text_temp)
-    text_temp = text_temp.split()
-    res=0
+    text_temp = text_temp.lower()
+    text_temp = text_temp.split()  
+
     for i in text_temp :
-        if i == word :
-            res=res+1 
-    return res
+        if i in resdict :
+            resdict[i] +=1
 
-print (wordCount(text , "However"))
+        else :
+            resdict[i] = 1
+    return resdict
 
+def maxValueInDict(dict_p):
+    if not dict_p : 
+        return None,None
+    res= 0
+    for i in dict_p.values() :
+        if i > res :
+            res = i
+
+    for j in dict_p.keys() :
+        if dict_p[j] == res :
+            res_j = j
+            break
+
+    return j,res
+
+def top10values(dict_p) :
+    dp_sorted =sorted(dict_p.items(), key=lambda t: t[1] , reverse=True)
+    for i in dict_p :
+        d = dp_sorted[:10]
+
+    return d
+
+
+ttr = urllib.request.urlopen("https://www.irit.fr/~Philippe.Muller/alice_wonderland.utf8.conll")
+text_pretreated = ttr.read().decode('utf-8')
+
+
+lines = text_pretreated.split("\n")
+print (lines)
+
+
+
+'''
+dictio= wordCount(text)
+print( maxValueInDict(dictio) )
+print( top10values(dictio))
+'''
 
 
